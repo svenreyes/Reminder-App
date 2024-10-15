c64fc02308f9efce706c46b96b2ba939bcdc068d
svenreyes <svenxx64@gmail.com>
2024-10-15 14:21:23 -0400

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [reminders, setReminders] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setReminders([...reminders, { name, message }]);
+    setName('');
+    setMessage('');
+  };
+
+  const handleDelete = (index) => {
+    const updatedReminders = reminders.filter((_, i) => i !== index);
+    setReminders(updatedReminders);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>Set Reminder</h1> 
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Title:</label>
+            <input className="input-field"
+            type="text" 
+            id="name" 
+            value={name} 
+            onChange={(e) => setName(e.target.value)} required />
+          </div>
+          <div htmlFor="message">
+            <lable>Message:</lable>
+            <input className="input-field"
+            type="text" 
+            id="message" 
+            value={message} 
+            onChange={(e) => setMessage(e.target.value)} required />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+
+        {reminders && reminders.length > 0 && (
+          <div className="reminders-container">
+          <h2>Reminders</h2>
+          <div className="reminders-list">
+            {reminders.map((reminder, index) => (
+              <div key={index} className="reminder-box">
+                <h3>{reminder.name}</h3>
+                <p>{reminder.message}</p>
+                <button 
+                    className="delete-button" 
+                    onClick={() => handleDelete(index)}
+                  >
+                    X
+                  </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </header>
+  </div>
+);
 }
 
 export default App;
